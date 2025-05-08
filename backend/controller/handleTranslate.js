036f652a8bf61db823d97cb676e7bc9efc2f528b
ralph12322 <gjcshs.santos.ralphgeo@gmail.com>
2025-05-08 15:00:18 +0800

    rerere

--- backend/controller/handleTranslate.js
+++ backend/controller/handleTranslate.js
@@ -2,7 +2,7 @@ import googleTranslateApi from '@vitalets/google-translate-api'; //using the @vi
 import axios from 'axios';
 
 export const translate = async (req, res) => {
-    const LINGVA_BASE_URL = 'https://lingva.ml';
+    const LINGVA_BASE_URL = 'https://lingva-translate-brw0.onrender.com';
     const { from, text, to } = req.body;
   
     if (!text || !to || !from) {
