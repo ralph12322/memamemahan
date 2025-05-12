2dec41f2113554e18a5874160127b7a7e7af2bf0
ralph12322 <gjcshs.santos.ralphgeo@gmail.com>
2025-05-13 01:00:27 +0800

    try

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,9 @@ app.use(cors({
   origin: ['https://emovox.vercel.app', 'http://localhost:5174'],
 }));
 
+app.options('*', cors()); // Enable preflight across-the-board
+
+
 
 
 app.use(express.json());
