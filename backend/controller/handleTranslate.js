244f7cc1a5acf178bbbb6c45b4638c890256e46d
James Talamo <jamestalamo4@gmail.com>
2025-05-14 19:44:14 +0800

    controller add

--- backend/controller/handleTranslate.js
+++ backend/controller/handleTranslate.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
-import {getEmotions, getEmotionsv2 } from '../lib/emotions.js';
+import { getEmotions, getEmotionsv2 } from '../lib/emotions.js';
 
 export const translate = async (req, res) => {
   const LINGVA_BASE_URL = 'https://lingva-translate-brw0.onrender.com';
   const { from, text, to } = req.body;
 
-  if(!LINGVA_BASE_URL) {
+  if (!LINGVA_BASE_URL) {
     console.log("mistake")
   }
 
@@ -18,7 +18,7 @@ export const translate = async (req, res) => {
       `${LINGVA_BASE_URL}/api/v1/${from}/${to}/${encodeURIComponent(text)}`
     );
     const translated = response.data?.translation;
-    
+
     if (!translated) {
       return res.status(500).json({ error: 'Failed to get translation' });
     }
@@ -26,10 +26,20 @@ export const translate = async (req, res) => {
     //this part handles the emotion analysis, if the twinword can't identify the emotion that's when we use the gpt4
     let result = await getEmotionsv2(text);
     //usually complex text gets hard for twinword api, such as long messages, complex words, etc.
-    if(!result || !result.emotion){
+    if (!result || !result.emotion) {
       result = await getEmotions(text);
-    } 
-    
+    }
+
+    //James added
+    if (!result || !result.emotion) {
+      return res.json({
+        translatedText: translated,
+        emotion: 'Api ERROR',
+        warning: 'Api ERROR',
+      });
+    }
+    //end
+
     res.json({ translatedText: translated, emotion: result.emotion, warning: result.source });
   } catch (error) {
     console.error('Translation error:', error);
