7b2848c2f34f7e9b702abccd1321fd13d619fada
ralph12322 <154263335+ralph12322@users.noreply.github.com>
2025-05-14 11:58:32 +0800

    Update handleTranslate.js

--- backend/controller/handleTranslate.js
+++ backend/controller/handleTranslate.js
@@ -24,7 +24,7 @@ export const translate = async (req, res) => {
     }
 
     //this part handles the emotion analysis, if the twinword can't identify the emotion that's when we use the gpt4
-    let result = await getEmotions(text);
+    let result = await getEmotionsv2(text);
     //usually complex text gets hard for twinword api, such as long messages, complex words, etc.
     // if(!result || !result.emotion){
     //   result = await getEmotions(text)
