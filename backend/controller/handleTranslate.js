c4d044c257bd30ccfc18449e1f096ef41c965bd5
ralph12322 <154263335+ralph12322@users.noreply.github.com>
2025-05-14 13:24:27 +0800

    Update handleTranslate.js

--- backend/controller/handleTranslate.js
+++ backend/controller/handleTranslate.js
@@ -28,6 +28,11 @@ export const translate = async (req, res) => {
     //usually complex text gets hard for twinword api, such as long messages, complex words, etc.
     if(!result || !result.emotion){
       result = await getEmotions(text)
+    } else{
+      result = {
+        emotion: "neutral",
+        source: "No API USED"
+      }
     }
     res.json({ translatedText: translated, emotion: result.emotion, warning: result.source });
   } catch (error) {
