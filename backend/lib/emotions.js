8bc529a6527e2b538d448fbbd32875232ce218c4
James Talamo <jamestalamo4@gmail.com>
2025-05-14 19:19:59 +0800

    getEmotionsV2 -james edited

--- backend/lib/emotions.js
+++ backend/lib/emotions.js
@@ -22,62 +22,63 @@ export const getEmotions = async (text) => {
 
         const message = "OPENAI used"
 
-        return {emotion : completion.choices[0].message.content, source: message }
+        return { emotion: completion.choices[0].message.content, source: message }
     } catch (error) {
         console.error('Error from OpenAI API:', error);
     }
 }
 
 export const getEmotionsv2 = async (text) => {
-	try {
-		const options = {
-			method: 'POST',
-			hostname: 'twinword-emotion-analysis-v1.p.rapidapi.com',
-			port: null,
-			path: '/analyze/',
-			headers: {
-				'x-rapidapi-key': process.env.RAPIDAPI_KEY,
-				'x-rapidapi-host': process.env.RAPIDAPI_HOST,
-				'Content-Type': 'application/x-www-form-urlencoded'
-			}
-		};
+    try {
+        const options = {
+            method: 'POST',
+            hostname: 'twinword-emotion-analysis-v1.p.rapidapi.com',
+            port: null,
+            path: '/analyze/',
+            headers: {
+                'x-rapidapi-key': process.env.RAPIDAPI_KEY,
+                'x-rapidapi-host': process.env.RAPIDAPI_HOST,
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        };
 
-		const emotion = await new Promise((resolve, reject) => {
-			const twinReq = https.request(options, (twinRes) => {
-				const chunks = [];
+        const emotion = await new Promise((resolve, reject) => {
+            const twinReq = https.request(options, (twinRes) => {
+                const chunks = [];
 
-				twinRes.on('data', (chunk) => chunks.push(chunk));
-				twinRes.on('end', () => {
-					try {
+
+                twinRes.on('data', (chunk) => chunks.push(chunk));
+                twinRes.on('end', () => {
+                    try {
                         const message = "TWINWORD used"
-						const body = Buffer.concat(chunks).toString();
-						const json = JSON.parse(body);
-					
-						console.log('Twinword response:', json);
+                        const body = Buffer.concat(chunks).toString();
+                        const json = JSON.parse(body);
 
-						if (json.emotions_detected && json.emotions_detected.length > 0) {
-							resolve({emotion: json.emotions_detected[0], source: message});
-						} 
-						else{
-							
-						}
-					} catch (err) {
-						reject(new Error('Failed to parse response as JSON'));
-					}
-				});
-			});
+                        console.log('Twinword response:', json);
 
-			twinReq.on('error', (err) => {
-				reject(err);
-			});
+                        if (json.emotions_detected && json.emotions_detected.length > 0) {
+                            resolve({ emotion: json.emotions_detected[0], source: message });
+                        }
+                        else {
+                            resolve(null); 
+                        }
+                    } catch (err) {
+                        reject(new Error('Failed to parse response as JSON'));
+                    }
+                });
+            });
 
-			twinReq.write(qsStringify({ text }));
-			twinReq.end();
+            twinReq.on('error', (err) => {
+                reject(err);
+            });
 
-		});
-	return emotion
-	} catch (error) {
-		console.error('Error from Twinword API:', error);
-		return "error";
-	}
+            twinReq.write(qsStringify({ text }));
+            twinReq.end();
+
+        });
+        return emotion
+    } catch (error) {
+        console.error('Error from Twinword API:', error);
+        return "error";
+    }
 };
